1053e105266b6958e390a2380373ed51e126dc9a
SimonImanse <56388490+SimonImanse@users.noreply.github.com>
2021-02-04 13:28:51 +0100

    Add files via upload

---
+++ Beroepsopdracht-klaar/quiz/quiz1.js
@@ -0,0 +1,77 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+        "title": "Lisser Art Museum",
+        "imageURI": "https://images3.persgroep.net/rcs/tEq_KObRoUyNsR3VxHAMMSaoQwY/diocontent/139016913/_fitwidth/763?appId=93a17a8fd81db0de025c8abd1cca1279&quality=0.8"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "In welke provincie ligt het Museum?",
+				"answers": [{
+						"answer": "Noord-Holland",
+						"feedback": false
+					},
+					{
+						"answer": "Zuid-Holland",
+						"feedback": true
+					},
+					{
+						"answer": "Brabant",
+						"feedback": false
+					},
+					{
+						"answer": "Friesland",
+						"feedback": false
+					},
+					{
+						"answer": "Groningen",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Wat is LAM voor museum?",
+				"answers": [{
+						"answer": "Street art",
+						"feedback": false
+					},
+					{
+						"answer": "Food art",
+						"feedback": true
+					},
+					{
+						"answer": "Colorfull art",
+						"feedback": false
+					},
+					{
+						"answer": "Kitsch art",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Wie is de oprichter van het Museum: ?",
+				"answers": [{
+						"answer": "Sem jansen",
+						"feedback": false
+					},
+					{
+						"answer": "Faas de jong",
+						"feedback": false
+					},
+					{
+						"answer": "Dirk ten have",
+						"feedback": false
+					},
+					{
+						"answer": "Jan van den broek",
+						"feedback": true
+					}
+				]
+			}
+		]
+	}
